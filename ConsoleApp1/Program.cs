bfa0217157f252aa93c94d796553da5511a2b092
johnkenny-coder <johnkennyalzona11@gmail.com>
2025-01-30 19:22:09 +0800

    Create Program.cs

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int attempts = 3;
+        while (attempts > 0)
+        {
+            if (Login())
+            {
+                Console.WriteLine("Login successful!");
+                break;
+            }
+            else
+            {
+                attempts--;
+                Console.WriteLine($"Incorrect Password You have {attempts} attempt left.");
+            }
+        }
+
+        if (attempts == 0)
+        {
+            Console.WriteLine("You've exceeded the maximum number of attempt. Please try again later.");
+        }
+    }
+
+    static bool Login()
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        if (username == "Admin" && password == "Admin1234!")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
